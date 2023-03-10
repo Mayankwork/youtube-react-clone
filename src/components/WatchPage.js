6eb52966139a299385546bf97f324a1efd857f6d
Mayankwork <guptamayankwork@gmail.com>
2023-03-10 12:28:43 +0530

    chanes

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -11,6 +11,6 @@ searchParams.get("v");
         dispatch(closeMenu());
     },[])
   return (
-    <div className='px-5'><iframe width="800" height="400" src={"https://www.youtube.com/embed/" + searchParams.get("v")} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe></div>
+    <div className='px-5'><iframe width="800" height="400" src={"https://www.youtube.com/embed/"  + searchParams.get("v")} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe></div>
   )
 }
